a54dfc0febb837b0f6dd0bb9ea48399fd92529ac
ayakadihaja <aya.kadihaja@gmail.com>
2025-11-03 16:16:19 +0100

    Add files via upload

---
+++ GrokPro/GrokPro-V1/frameworks/karate/grokpro-project/SRC/src/test/java/com/example/karate/TestRunner.java
@@ -0,0 +1,13 @@
+
+package com.example.karate;
+
+import com.intuit.karate.junit5.Karate;
+
+class TestRunner {
+
+  @Karate.Test
+  Karate testAll() {
+    return Karate.run("classpath:features");
+  }
+
+}
